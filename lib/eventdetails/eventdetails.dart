8b836d6c2eef833cf0f7aa4384fa8ce10bf93387
Sebastiano Poggi <poggos@gmail.com>
2017-03-26 18:04:54 +0100

    Show speakers

--- lib/eventdetails/eventdetails.dart
+++ lib/eventdetails/eventdetails.dart
@@ -10,14 +10,16 @@ class EventDetailsPage extends StatefulWidget {
 }
 
 class _EventDetailsPageState extends State<EventDetailsPage> {
+  _EventDetailsPageState(this.event);
+
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
   final Event event;
 
-  _EventDetailsPageState(this.event);
+  ThemeData theme;
 
   @override
   Widget build(BuildContext context) {
-    var flexibleSpaceBar = new FlexibleSpaceBar(
-        title: titleWidgetFrom(event.title, event.speakers));
+    theme = Theme.of(context);
 
     var appBar = new SliverAppBar(
       leading: new BackButton(),
@@ -25,29 +27,47 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
         new IconButton(
             icon: const Icon(Icons.search), onPressed: _handleSearchPress)
       ],
-      flexibleSpace: flexibleSpaceBar,
     );
 
     var rootScrollView = new CustomScrollView(slivers: [
       appBar,
       new SliverList(
-          delegate:
-              new SliverChildListDelegate([]))
+          delegate: new SliverChildListDelegate([
+        new Container(
+          decoration: new BoxDecoration(backgroundColor: theme.primaryColor),
+          child: new Padding(
+              padding: new EdgeInsets.fromLTRB(72.0, 8.0, 8.0, 8.0),
+              child: new Column(
+                  children: speakersChildrenFor(event.speakers),
+                  crossAxisAlignment: CrossAxisAlignment.start)),
+        )
+      ]))
     ]);
 
-    return new Scaffold(body: rootScrollView);
+    return new Scaffold(key: scaffoldKey, body: rootScrollView);
   }
 
-  List<Widget> speakersChildrenFor(List<Speaker> speakers) =>
-      speakers.map((speaker) {
-        return new Text(speaker.fullName);
-      }).toList(growable: false);
+  List<Widget> speakersChildrenFor(List<Speaker> speakers) {
+    var speakerNames = speakers.map((speaker) => speaker.fullName).join(", ");
+    return <Widget>[
+      new Row(
+          children: speakers
+              .map((speaker) => new CircleAvatar(
+                  backgroundImage: new NetworkImage(speaker.avatarUrl),
+                  radius: 20.0))
+              .map((widget) => new Padding(
+                  padding: new EdgeInsets.only(right: 8.0), child: widget))
+              .toList()),
+      new Padding(
+          padding: new EdgeInsets.only(top: 8.0),
+          child: new Text(speakerNames,
+              style: theme.textTheme.display1
+                  .copyWith(color: Colors.white, fontSize: 14.0, height: 1.714285714))),
+    ];
+  }
 
-  _handleSearchPress() {}
-
-  Widget titleWidgetFrom(String title, List<Speaker> speakers) =>
-      new Column(children: [
-        new Text(title),
-        new Row(children: speakersChildrenFor(speakers)),
-      ]);
+  _handleSearchPress() {
+    scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text("¯\\_(ツ)_/¯")));
+  }
 }
