5a0178c0305ac2b382d12521b7917cd31dcab28d
Sebastiano Poggi <poggos@gmail.com>
2017-03-26 17:24:54 +0100

    Define collapsing appbar behaviour

--- lib/eventdetails/eventdetails.dart
+++ lib/eventdetails/eventdetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'event.dart';
 
 class EventDetailsPage extends StatefulWidget {
@@ -15,16 +16,38 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(leading: new BackButton(), actions: [
+    var flexibleSpaceBar = new FlexibleSpaceBar(
+        title: titleWidgetFrom(event.title, event.speakers));
+
+    var appBar = new SliverAppBar(
+      leading: new BackButton(),
+      actions: [
         new IconButton(
-            icon: const Icon(Icons.search), onPressed: _handleSearchPress())
-      ]),
-      body: new Center(),
+            icon: const Icon(Icons.search), onPressed: _handleSearchPress)
+      ],
+      flexibleSpace: flexibleSpaceBar,
     );
+
+    var rootScrollView = new CustomScrollView(slivers: [
+      appBar,
+      new SliverList(
+          delegate:
+              new SliverChildListDelegate([]))
+    ]);
+
+    return new Scaffold(body: rootScrollView);
   }
 
-  _handleSearchPress() {
-    
-  }
+  List<Widget> speakersChildrenFor(List<Speaker> speakers) =>
+      speakers.map((speaker) {
+        return new Text(speaker.fullName);
+      }).toList(growable: false);
+
+  _handleSearchPress() {}
+
+  Widget titleWidgetFrom(String title, List<Speaker> speakers) =>
+      new Column(children: [
+        new Text(title),
+        new Row(children: speakersChildrenFor(speakers)),
+      ]);
 }
