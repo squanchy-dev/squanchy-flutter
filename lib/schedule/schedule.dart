f7fd14f446841e71b733d7b8a3b2de52be4b2605
Niamh Power <niamhpower@live.com>
2018-03-16 16:47:16 +0000

    add image and text for empty state

--- lib/schedule/schedule.dart
+++ lib/schedule/schedule.dart
@@ -18,14 +18,19 @@ class ScheduleView extends StatelessWidget {
     );
 
     final emptyState = new Container(child:
-      new Image.asset("assets/nothing_here.png")
+      new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            new Image.asset("assets/nothing_here.png"),
+            new Text('Nothing here. Come back soon!',
+              textAlign: TextAlign.center,
+              style: theme.textTheme.display1.copyWith(color: Colors.black, fontSize: 16.0),),
+          ],
+      )
     );
-
-    final emptyStateLabel = new Text("test");
 
     final body = new PageView(children: <Widget>[
       emptyState,
-      emptyStateLabel
     ],);
 
     return new Scaffold(
