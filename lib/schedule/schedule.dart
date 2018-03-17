5a648442aa91c1cdb0b6bce06cf6b592b3bc0b30
Niamh Power <niamhpower@live.com>
2018-03-17 17:43:16 +0000

    display days in list

--- lib/schedule/schedule.dart
+++ lib/schedule/schedule.dart
@@ -31,25 +31,38 @@ class EventCard extends StatelessWidget {
   }
 }
 
-Widget _printDayStuff(DocumentSnapshot doc) {
+_printDayStuff(DocumentSnapshot doc) {
   print('${doc['day']}');
   var day = new Day(doc['day']);
   print('${day.id}');
-  return List<Day>();
+}
+
+class EventList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var docs = Firestore.instance.collection('views').document('schedule').getCollection('schedule_pages').getDocuments().asStream();
+
+    return new StreamBuilder<QuerySnapshot>(
+      stream: docs,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) return const Text('Loading...');
+        return new ListView(
+          children: snapshot.data.documents.map((DocumentSnapshot document) {
+            var day = new Day(document['day']);
+            return new ListTile(
+              title: new Text(day.dateTime.toIso8601String()),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
 }
 
 class ScheduleView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-
-    var docs = Firestore.instance.collection('views').document('schedule').getCollection('schedule_pages').getDocuments();
-    docs.then((data) => _printDayStuff(data.documents[0]));
-
-    _list = new ListModel<Event>(
-      listKey: '',
-      initialItems: <Event>[],
-    );
 
     var appBar = new AppBar(
         iconTheme: theme.iconTheme,
@@ -81,7 +94,7 @@ class ScheduleView extends StatelessWidget {
 
     return new Scaffold(
       appBar: appBar,
-      body: body,
+      body: new EventList(),
       backgroundColor: theme.backgroundColor,
     );
   }
