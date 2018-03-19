2200ff82fa48f2cc2a109d69366103e933bcddd6
Niamh Power <niamhpower@live.com>
2018-03-19 18:45:39 +0000

    small changes

--- lib/schedule/schedule.dart
+++ lib/schedule/schedule.dart
@@ -4,6 +4,50 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class Card extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return new Container(
+        margin: new EdgeInsets.fromLTRB(76.0, 16.0, 16.0, 16.0),
+    constraints: new BoxConstraints.expand(),
+    child: new Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+    new Container(height: 4.0),
+    new Text('test',
+    style: theme.textTheme.display1,
+    ),
+    new Container(height: 10.0),
+    new Text('test 2',
+    style: theme.textTheme.display4
+    ),
+    new Container(
+    margin: new EdgeInsets.symmetric(vertical: 8.0),
+    height: 2.0,
+    width: 18.0,
+    color: new Color(0xff00c6ff)
+    ),
+    new Row(
+    children: <Widget>[
+    new Image.asset("assets/img/ic_distance.png", height: 12.0),
+    new Container(width: 8.0),
+    new Text('100',
+    style: theme.textTheme.display3,
+    ),
+    new Container(width: 24.0),
+    new Image.asset("assets/img/ic_gravity.png", height: 12.0),
+    new Container(width: 8.0),
+    new Text(planet.gravity,
+    style: regularTextStyle,
+    ),
+    ],
+    ),
+    ],
+    ),
+  });
+}
+
 class EventList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
